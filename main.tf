cc53c5d55bc6c3fb6baadca2032ca3ecce4641b7
Stefan Stieber <s.stieber@me.com>
2021-02-13 13:02:41 +0100

    add new hetzner server for jitsi app

---
+++ main.tf
@@ -0,0 +1,29 @@
+# name of the cloud provider
+terraform {
+    required_providers {
+        hcloud = {
+            source = "hetznercloud/hcloud"
+            version = "1.24.1"
+        }
+    }
+    backend "remote" {
+        organization = "doublest"
+        workspaces {
+            name = "infra-jitsi"
+        }
+    }
+}
+
+variable "hcloud_token" {}
+
+provider "hcloud" {
+    token = var.hcloud_token
+}
+
+# create server for habor registry
+resource "hcloud_server" "jitsi-server" {
+  name        = "habor"
+  server_type = "cx11"
+  image       = "ubuntu-20.04"
+  location    = "nbg1"
+}
